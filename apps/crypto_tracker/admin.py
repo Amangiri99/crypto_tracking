812aaa50d33b687638579a0247ed0aefdb144ed4
Aman Giri <ansugiri14@gmail.com>
2024-04-28 11:54:18 +0530

    Created a model to store the details about the crypto coins

--- apps/crypto_tracker/admin.py
+++ apps/crypto_tracker/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from apps.crypto_tracker import models as cryptp_tracker_models
+
 # Register your models here.
+admin.site.register(cryptp_tracker_models.CryptoDetail)
